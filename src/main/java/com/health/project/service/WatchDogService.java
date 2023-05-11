4be7903bcb6ff0eac8e81276783fed52d3bd60f0
손종국 <kyky7852@naver.com>
2023-05-11 18:02:37 +0900

    VM argument로 processName 받은 후 Name에 따라 체크

--- src/main/java/com/health/project/service/WatchDogService.java
+++ src/main/java/com/health/project/service/WatchDogService.java
@@ -1,7 +1,9 @@
 package com.health.project.service;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,22 +20,25 @@ import lombok.extern.slf4j.Slf4j;
 public class WatchDogService {
 	
 	
-	@Value("${cloud.server.id}")
+	@Value("${dev.server.id}")
 	 String id;
 	
-	@Value("${cloud.server.pw}")
+	@Value("${dev.server.pw}")
 	 String pw;
 	
-	@Value("${cloud.server.ip}")
+	@Value("${dev.server.ip}")
 	 String ip;
 	
-	@Value("${cloud.server.port}")
+	@Value("${dev.server.port}")
 	 int port;
+	
+	@Value("${process.name}")
+	String processName ;
 
 	
-public HashMap<String, Boolean> serverConnect() throws Exception{
+public HashMap<String, Object> serverConnect() throws Exception{
 		
-		log.info("서버 프로세스 확인 중...");
+		log.info("서버 프로세스 확인 중..... " + processName);
 		
 		HashMap<String, Boolean> hashMap = new HashMap<String, Boolean>();								
 		
@@ -54,7 +59,7 @@ public HashMap<String, Boolean> serverConnect() throws Exception{
         Channel channel = session.openChannel("exec");  // 채널접속  exec = 명령을 요청하기 위함 
         ChannelExec channelExec = (ChannelExec) channel; // 명령 전송 채널사용
         channelExec.setPty(true);
-        channelExec.setCommand("ps -ef | grep java"); // 내가 실행시킬 명령어를 입력
+        channelExec.setCommand("ps -ef | grep java"); // 실행시킬 명령어를 입력
       
         // 명령어의 실행 결과를 받을 준비.
         StringBuilder outputBuffer = new StringBuilder();
@@ -72,26 +77,45 @@ public HashMap<String, Boolean> serverConnect() throws Exception{
             }
             // 명령어의 실행이 끝나면, 실행 결과를 분석
             if (channel.isClosed()) {            	            	
-            	                
-                boolean pointer = outputBuffer.toString().contains("-DSERVICE_NAME=pointerd");
-                boolean pointer_example = outputBuffer.toString().contains("-DSERVICE_NAME=pointer_exampled");
-                boolean poodle = outputBuffer.toString().contains("-DSERVICE_NAME=poodle");
-                
-                hashMap.put("pointer", pointer);
-                hashMap.put("pointer_example", pointer_example);
-                hashMap.put("poodle", poodle);
+            	
+            	List<String> list = Arrays.asList(processName.split("\\|"));
+            	HashMap<String, Object> map = new HashMap<String, Object>();
+            	String serviceName = null;
+            	String proccessYN = null;
+            	
+            	for(int i = 0 ; list.size() > i; i++) {
+            		serviceName = "serviceName_"+i;
+            		proccessYN = "process_"+i;
+            		System.out.println("체크 할 ServiceName --------> " + list.get(i));
+            		
+            		map.put(serviceName , list.get(i));
+            		map.put(proccessYN , outputBuffer.toString().contains("-DSERVICE_NAME="+list.get(i)));
+            	
+            	}
+            	
+            	for(int i = 0 ; list.size() > i; i++) {
+            		serviceName = "serviceName_"+i;
+            		proccessYN = "process_"+i;            		
+            		
+            		System.out.println( map.get(serviceName) + " 동작 상태 --------> " + map.get(proccessYN));
+            		            		
+            	}
+            	
+//                boolean pomeranian_sso = outputBuffer.toString().contains("-DSERVICE_NAME=pomeranian_sso");
+//                boolean pomeranian_gasmon_m = outputBuffer.toString().contains("-DSERVICE_NAME=pomeranian_gasmon_m");
+//                boolean heavymachine = outputBuffer.toString().contains("-DSERVICE_NAME=pomeranian_heavymachine");
+//                
+//                hashMap.put("pomeranian_sso", pomeranian_sso);
+//                hashMap.put("pomeranian_gasmon_m", pomeranian_gasmon_m);
+//                hashMap.put("heavymachine", heavymachine);
                                                 
-                log.info("서버 명령어 결과 ------> " + outputBuffer.toString());                               
+//                log.info("서버 명령어 결과 ------> " + outputBuffer.toString());                               
                                             
                 // SSH 연결을 종료하고, 결과값을 반환
                 channel.disconnect();
                 session.disconnect();
-                
-                // 서버가 꺼져있을 시 재기동 
-//                if(!bool) {
-//                	result = this.ifProcDown();
-//                }                            
-                return hashMap;                
+                                             
+                return map;                
             }      
         }  
 			
@@ -102,73 +126,141 @@ public HashMap<String, Boolean> serverConnect() throws Exception{
 	 * @return
 	 * @throws Exception
 	 */
-	public boolean ifProcDown(String serverName) throws Exception{
+	public boolean restart(String serverName) throws Exception{
 		
 		log.info("프로세스 다운 재기동... "+ serverName);
-				
-//		
-//		try {
-//							
-//		// JSch 라이브러리로 SSH 세션을 생성
-//        JSch jsch = new JSch();
-//        Session session = jsch.getSession(id, ip , port);
-//        session.setPassword(pw);
-//        
-//        // 호스트 키 확인을 생략.
-//        java.util.Properties config = new java.util.Properties();
-//        config.put("StrictHostKeyChecking", "no");
-//        session.setConfig(config);
-//        
-//        // SSH 연결을 시도.
-//        session.connect();  
-//        
-//        // SSH 채널을 열고, 실행할 명령어를 설정.
-//        Channel channel = session.openChannel("exec");  // 채널접속  exec = 명령을 요청하기 위함 
-//        ChannelExec channelExec = (ChannelExec) channel; // 명령 전송 채널사용
-//        channelExec.setPty(true);
-//        
-//       
-//        // 내가 실행시킬 명령어를 입력
-//        channelExec.setCommand("API-Server/shell/javaStart.sh");
-//        
-//        // 명령어의 실행 결과를 받을 준비를 합니다.
-//        StringBuilder outputBuffer = new StringBuilder();
-//        InputStream in = channel.getInputStream();
-//        ((ChannelExec) channel).setErrStream(System.err); 
-//        
-//        // 명령어를 실행하고, 결과를 받아옴.
-//        channel.connect();  //실행
-//        byte[] tmp = new byte[1024];
-//        while (true) {
-//            while (in.available() > 0) {
-//                int i = in.read(tmp, 0, 1024);
-//                outputBuffer.append(new String(tmp, 0, i));
-//                if (i < 0) break;
-//            }
-//            // 명령어의 실행이 끝나면, 실행 결과를 분석
-//            if (channel.isClosed()) {
-//            	            	           	
-//              boolean bool = outputBuffer.toString().contains("successfully");     
-//                                                        
-//               if(bool) {
-//               	result = "R";
-//               }else {
-//               	result = "N";
-//               }
-//               
-//                // SSH 연결을 종료하고, 결과값을 반환
-//                channel.disconnect();
-//                session.disconnect();
-//                return result;
+		boolean bool = false ;						
+							
+		// JSch 라이브러리로 SSH 세션을 생성
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(id, ip , port);
+        session.setPassword(pw);
+        
+        // 호스트 키 확인을 생략.
+        java.util.Properties config = new java.util.Properties();
+        config.put("StrictHostKeyChecking", "no");
+        session.setConfig(config);
+        
+        // SSH 연결을 시도.
+        session.connect();  
+        
+        // SSH 채널을 열고, 실행할 명령어를 설정.
+        Channel channel = session.openChannel("exec");  // 채널접속  exec = 명령을 요청하기 위함 
+        ChannelExec channelExec = (ChannelExec) channel; // 명령 전송 채널사용
+        channelExec.setPty(true);
+        
+        // 실행시킬 명령어를 입력
+        if(serverName.equals("pomeranian_sso")) {
+        	log.info(serverName+" shell 실행-------> 1");
+        	channelExec.setCommand("cd /neonexsoft/apps/iwest/pomeranian_sso/bin ; ./tomcat.sh start");	
+        }else if (serverName.equals("pomeranian_gasmon_m")) {
+        	log.info(serverName+" shell 실행-------> 2");
+        	channelExec.setCommand("cd /neonexsoft/apps/iwest/pomeranian_gasmon_m/bin ; ./tomcat.sh start");
+        }else if (serverName.equals("pomeranian_heavymachine")) {
+        	log.info(serverName+" shell 실행-------> 3");
+        	channelExec.setCommand("cd /neonexsoft/apps/iwest/pomeranian_heavymachine/bin/ ; ./tomcat.sh start");
+        }
+          
+        
+        // 명령어의 실행 결과를 받을 준비를 합니다.
+        StringBuilder outputBuffer = new StringBuilder();
+        InputStream in = channel.getInputStream();
+        ((ChannelExec) channel).setErrStream(System.err); 
+        
+        // 명령어를 실행하고, 결과를 받아옴.
+        channel.connect();  //실행
+        byte[] tmp = new byte[1024];
+        while (true) {
+            while (in.available() > 0) {
+                int i = in.read(tmp, 0, 1024);
+                outputBuffer.append(new String(tmp, 0, i));
+                if (i < 0) break;
+            }
+            // 명령어의 실행이 끝나면, 실행 결과를 분석
+            if (channel.isClosed()) {
+                
+//            	
+//            	boolean pomeranian_sso = outputBuffer.toString().contains("-DSERVICE_NAME=pomeranian_sso");
+//            	boolean pomeranian_gasmon_m = outputBuffer.toString().contains("-DSERVICE_NAME=pomeranian_gasmon_m");
+//                boolean heavymachine = outputBuffer.toString().contains("-DSERVICE_NAME=heavymachine");     
 //                
-//            }      
-//        } 
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				 result = "N";
-//				 return result;			
-//			}
-		return false;
+//                
+//                if(pomeranian_sso | pomeranian_gasmon_m | heavymachine) {
+//                	bool = true;
+//                }
+                           
+                // SSH 연결을 종료하고, 결과값을 반환
+                channel.disconnect();
+                session.disconnect();                               
+            }      
+            Thread.sleep(25000); // 25초
+            return restartCheck(serverName);
+        } 
+				
+    }
+	
+	
+	
+public boolean restartCheck(String serverName) throws Exception{
+		
+		log.info("프로세스 재기동 체크 ... "+ serverName);
+								
+							
+		// JSch 라이브러리로 SSH 세션을 생성
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(id, ip , port);
+        session.setPassword(pw);
+        
+        // 호스트 키 확인을 생략.
+        java.util.Properties config = new java.util.Properties();
+        config.put("StrictHostKeyChecking", "no");
+        session.setConfig(config);
+        
+        // SSH 연결을 시도.
+        session.connect();  
+        
+        // SSH 채널을 열고, 실행할 명령어를 설정.
+        Channel channel = session.openChannel("exec");  // 채널접속  exec = 명령을 요청하기 위함 
+        ChannelExec channelExec = (ChannelExec) channel; // 명령 전송 채널사용
+        channelExec.setPty(true);
+        
+        // 실행시킬 명령어를 입력
+        
+        channelExec.setCommand("ps -ef | grep "+ serverName);	
+                      
+        
+        // 명령어의 실행 결과를 받을 준비를 합니다.
+        StringBuilder outputBuffer = new StringBuilder();
+        InputStream in = channel.getInputStream();
+        ((ChannelExec) channel).setErrStream(System.err); 
+        
+        // 명령어를 실행하고, 결과를 받아옴.
+        channel.connect();  //실행
+        byte[] tmp = new byte[1024];
+        while (true) {
+            while (in.available() > 0) {
+                int i = in.read(tmp, 0, 1024);
+                outputBuffer.append(new String(tmp, 0, i));
+                if (i < 0) break;
+            }
+            // 명령어의 실행이 끝나면, 실행 결과를 분석
+            if (channel.isClosed()) {
+                
+            	
+            	boolean bool = outputBuffer.toString().contains("-DSERVICE_NAME="+serverName);
+            	System.out.println("-DSERVICE_NAME="+serverName);
+            	
+            	System.out.println("outputBuffer.toString()--> " + outputBuffer.toString());
+                           
+                // SSH 연결을 종료하고, 결과값을 반환
+                channel.disconnect();
+                session.disconnect();
+                
+                return bool;
+                
+            }      
+        } 
+				
     }
 }
 

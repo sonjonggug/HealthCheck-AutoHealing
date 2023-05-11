d92591946ba8b9a9493e7e2fe0a3414783df98aa
손종국 <kyky7852@naver.com>
2023-05-11 18:07:14 +0900

    메일 전송시 IP , Port , ProcessName , 실패 경과 시간 추가해서 발송

--- src/main/java/com/health/project/service/SendService.java
+++ src/main/java/com/health/project/service/SendService.java
@@ -1,5 +1,6 @@
 package com.health.project.service;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +12,34 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class SendService {
 	
-	@Value("${cloud.server.ip}")
+	@Value("${dev.server.ip}")
 	 String ip;
 	
-	@Value("${cloud.server.port}")
+	@Value("${dev.server.port}")
 	 int port;
 	
 	
-	public boolean SendMail(int SendSeq , String serverName) {
-		log.info("메일 발송 -------> ");
+	
+	
+	public boolean SendRestartSuccess(int SendSeq , String serverName) {
 		
+		if(SendSeq == 1) {
+			log.info("메일 발송 -------> " + ip + ":" + port + " 에 " + serverName + " " + Constans.RESTART_SUCESS);
+		}else {
+			log.info("메일 발송 -------> " + ip + ":" + port + " 에 " + serverName + " " + Constans.RESTART_FAIL + " (경과시간 " + SendSeq + "0분)");
+		}
 		
-		log.info(ip + ":" + port + " 에 " + serverName + " " + Constans.RESTART_FAIL);
+		return true;
+	}
+	
+	public boolean SendRestartFail(int SendSeq , String serverName) {
+		
+		if(SendSeq == 1) {
+			log.info("메일 발송 -------> " + ip + ":" + port + " 에 " + serverName + " " + Constans.RESTART_SUCESS );
+		}else {
+			log.info("메일 발송 -------> " + ip + ":" + port + " 에 " + serverName + " " + Constans.RESTART_FAIL + " (경과시간 " + SendSeq + "0분)");
+		}
+						
 		
 		return true;
 	}
